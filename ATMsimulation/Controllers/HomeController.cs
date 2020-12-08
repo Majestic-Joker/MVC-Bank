0fb33ef8a8a9a38b1fee0c6e9d1c0d8d6528a92e
Joshua <joshua.a.beaty@gmail.com>
2020-12-08 05:09:48 -0600

    Controllers - Account, Checking Account, Home, Manage, Transaction

---
+++ ATMsimulation/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ATMsimulation.Models;
+using Microsoft.AspNet.Identity;
+
+namespace ATMsimulation.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        
+        //GET /home/index
+        [Authorize]
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+            var checkingAccountId = db.CheckingAccounts.Where(c => c.ApplicationUserId == userId).First().Id;
+            ViewBag.CheckingAccountId = checkingAccountId;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.TheMessage = "Having trouble? Send us a message.";
+
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Contact(string message)
+        {
+            //TODO : Send message to HQ
+            ViewBag.TheMessage = "Thanks, we got your message!";
+            return PartialView("_ContactThanks");
+        }
+
+        public ActionResult Serial(string letterCase)
+        {
+            var serial = "ASPNETMVC5ATM1";
+            if(letterCase == "lower")
+            {
+                return Content(serial.ToLower());
+            }
+            return Content(serial);
+        }
+    }
+}
