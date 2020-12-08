7e063e0fd312e4d95f38fc229a43ff1eb42316fd
Joshua <joshua.a.beaty@gmail.com>
2020-12-08 05:07:53 -0600

    startup/web config etc.

---
+++ ATMsimulation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ATMsimulation.Startup))]
+namespace ATMsimulation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
